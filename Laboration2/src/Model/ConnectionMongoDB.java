ac0ad7a1fa5caf6ce1d7a8362b1daa91ec961756
carlitos84 <cgaldo@kth.se>
2015-12-14 17:29:47 +0100

    Add artist

--- Laboration2/src/Model/ConnectionMongoDB.java
+++ Laboration2/src/Model/ConnectionMongoDB.java
@@ -5,6 +5,7 @@
  */
 package Model;
 import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.BulkWriteOperation;
 import com.mongodb.BulkWriteResult;
 import com.mongodb.Cursor;
@@ -15,18 +16,23 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.ParallelScanOptions;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import org.bson.Document;
 
 /**
  *
  * @author Teddy
  */
-public class ConnectionMongoDB {
+public class ConnectionMongoDB implements InterfaceMongoDB{
     private DB db;
+    MongoClient mongoClient;
     public ConnectionMongoDB() throws UnknownHostException
     {
        /*
@@ -41,9 +47,58 @@ public class ConnectionMongoDB {
         MongoClient mongoClient = new MongoClient(Arrays.asList(new ServerAddress("localhost", 27017),
                                               new ServerAddress("localhost", 27018),
                                               new ServerAddress("localhost", 27019)));*/
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+        mongoClient = new MongoClient( "localhost" , 27017 );
 
         db = mongoClient.getDB( "labb2" ); //labb2 = database
+        
+        //addArtist("Green Teddy","Chile");
+    }
+    
+    //Test
+    private void getArtist() {
+        
+        DBCollection coll = db.getCollection("artist");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Search for artist: ");
+        String queryStr = scan.nextLine();
+        BasicDBObject query = new BasicDBObject("name", queryStr);
+        DBCursor cursor = coll.find(query);
+        try {
+            while(cursor.hasNext() ) {
+                BasicDBObject obj = (BasicDBObject)cursor.next();
+                System.out.println(obj.getString("nationality"));
+            }
+        } finally {
+            cursor.close();
+        }
+        //Document doc = new Document( db.getCollection("artist".find()))
+        mongoClient.close();
+        
+    }
+
+    @Override
+    public ArrayList<MadeBy> searchForString(String searchString) {
+       return null; 
+    }
+
+    @Override
+    public void addArtist(String name, String nationality) {
+        DBCollection coll = db.getCollection("artist");
+        BasicDBObject artist = new BasicDBObject();
+        artist.put("name", name);
+        artist.put("nationality", nationality);
+        coll.insert(artist);
+        mongoClient.close(); //För test
+    }
+
+    @Override
+    public void addAlbum(String title, String genre, String artistName, int date) throws ArtistDoesNotExistException {
+        
+    }
+
+    @Override
+    public void rateAlbum(int userId, int albumId, int score) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
